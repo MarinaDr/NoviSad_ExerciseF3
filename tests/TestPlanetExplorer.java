76430d03ae3fad90777eb35f296070919822b9a4
somename <someemail>
2017-05-12 20:22:31 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,22 +8,20 @@ public class TestPlanetExplorer {
 	public void test_executeCommand() {
 		fail("Not yet implemented");
 	}*/
+	PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 	
 	@Test
 	public void test_Initialize() throws PlanetExplorerException {
-		PlanetExplorer plnt=new PlanetExplorer(3, 3, "(1,1)");
 		
 		assertEquals("Incorrect position when initialize", "(0,0,N)", plnt.executeCommand(null));
 	}
 	
 	public void test_moving_forwoard() throws PlanetExplorerException {
-PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 		
 		assertEquals("Incorrect position when moving forwoard", "(1,0,N)", plnt.executeCommand("f"));
 	}
 	
 	public void test_moving_backwoard() throws PlanetExplorerException {
-		PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 				
 				assertEquals("Incorrect position when moving forwoard", "(0,0,N)", plnt.executeCommand("b"));
 			}
