c5d871f803c36d7ceffefc10c8ad7f350f7a6b17
somename <someemail>
2017-05-12 20:15:13 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -13,6 +13,6 @@ public class TestPlanetExplorer {
 	public void test_Initialize() throws PlanetExplorerException {
 		PlanetExplorer plnt=new PlanetExplorer(3, 3, "(1,1)");
 		
-		assertEquals("Incorrect position when initialize", "(0,0,N)", plnt.executeCommand());
+		assertEquals("Incorrect position when initialize", "(0,0,N)", plnt.executeCommand(null));
 	}
 }
