8e911283ed7c6ef1d490b55110ac5288930801b6
somename <someemail>
2017-05-12 20:39:15 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -17,55 +17,66 @@ public class TestPlanetExplorer {
 		assertEquals("Incorrect position when initialize", "(0,0,N)", plnt.executeCommand(null));
 	}
 	
+	@Test
 	public void test_moving_forwoard() throws PlanetExplorerException {
 		PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 		
 		assertEquals("Incorrect position when moving forwoard", "(1,0,N)", plnt.executeCommand("f"));
 	}
-	
+	@Test
 	public void test_moving_backwoard() throws PlanetExplorerException {
 		PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 		
 				assertEquals("Incorrect position when moving forwoard", "(0,0,N)", plnt.executeCommand("b"));
 			}
+	@Test
 	public void test_moving_right_when_north() throws PlanetExplorerException {
 		PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 		
 		assertEquals("Incorrect position when moving forwoard", "(0,0,E)", plnt.executeCommand("r"));
 	}
+	@Test
 public void test_moving_right_when_east() throws PlanetExplorerException {
 	PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 	
 		assertEquals("Incorrect position when moving forwoard", "(0,0,W)", plnt.executeCommand("r"));
 	}
+	@Test
 public void test_moving_right_when_south() throws PlanetExplorerException {
 	PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 	
 	assertEquals("Incorrect position when moving forwoard", "(0,0,S)", plnt.executeCommand("r"));
 }
+	@Test
 public void test_moving_right_when_west() throws PlanetExplorerException {
 	PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 	
 	assertEquals("Incorrect position when moving forwoard", "(0,0,N)", plnt.executeCommand("r"));
 }
+	@Test
 public void test_moving_left_when_north() throws PlanetExplorerException {
 	PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 	
 	assertEquals("Incorrect position when moving forwoard", "(0,0,W)", plnt.executeCommand("l"));
 }
+	@Test
 public void test_moving_left_when_east() throws PlanetExplorerException {
 	PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 	
 	assertEquals("Incorrect position when moving forwoard", "(0,0,N)", plnt.executeCommand("l"));
 }
+	@Test
 public void test_moving_left_when_south() throws PlanetExplorerException {
 	PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 	
 	assertEquals("Incorrect position when moving forwoard", "(0,0,E)", plnt.executeCommand("l"));
 }
+	@Test
 public void test_moving_left_when_west() throws PlanetExplorerException {
 	PlanetExplorer plnt=new PlanetExplorer(3, 3, "(2,2)");
 	
 	assertEquals("Incorrect position when moving forwoard", "(0,0,S)", plnt.executeCommand("l"));
 }
+
+
 }
